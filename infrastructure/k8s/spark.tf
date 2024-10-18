6a723313edc1d1b9cb147e970e2ea4c9bf60a272
rafaelsmf <rafael.sousamf@gmail.com>
2024-10-18 03:41:32 -0300

    feat: adding spark-operator

---
+++ infrastructure/k8s/spark.tf
@@ -0,0 +1,24 @@
+# Spark Namespace
+resource "kubectl_manifest" "spark_operator_namespace" {
+  yaml_body = file("./manifests/spark-operator-namespace.yaml")
+}
+
+# Spark ServiceAccount
+resource "kubectl_manifest" "spark_operator_serviceaccount" {
+  yaml_body = file("./manifests/spark-operator-serviceaccount.yaml")
+}
+
+# Spark ClusterRoleBinding
+resource "kubectl_manifest" "spark_operator_clusterrolebinding" {
+  yaml_body = file("./manifests/spark-operator-clusterrolebinding.yaml")
+}
+
+# Add Spark-Operator using Helm
+resource "helm_release" "spark" {
+  depends_on = [kubectl_manifest.spark_operator_namespace, kubectl_manifest.spark_operator_serviceaccount, kubectl_manifest.spark_operator_clusterrolebinding]
+  name       = "spark"
+  repository = "https://kubeflow.github.io/spark-operator"
+  chart      = "spark-operator"
+  namespace  = kubectl_manifest.spark_operator_namespace.name
+  wait       = false
+}
