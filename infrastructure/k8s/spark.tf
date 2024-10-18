3a213c03d497c71b2b63a75774913c4189454277
rafaelsmf <rafael.sousamf@gmail.com>
2024-10-18 19:49:45 -0300

    fix: resolve issue with spark-operator version.

--- infrastructure/k8s/spark.tf
+++ infrastructure/k8s/spark.tf
@@ -1,36 +1,27 @@
 # Spark Namespace
-resource "kubectl_manifest" "spark_operator_namespace" {
-  yaml_body = file("./manifests/spark-operator-namespace.yaml")
+resource "kubectl_manifest" "spark-namespace" {
+  yaml_body = file("${path.module}/manifests/spark-namespace.yaml")
 }
 
-# Spark ServiceAccount
-resource "kubectl_manifest" "spark_operator_serviceaccount" {
-  yaml_body = file("./manifests/spark-operator-serviceaccount.yaml")
+# Spark Service account and Cluster Role Binding
+data "kubectl_path_documents" "spark-service-account" {
+  pattern = "${path.module}/manifests/spark-service-account.yaml"
 }
 
-# Spark ClusterRole
-resource "kubectl_manifest" "spark_operator_clusterrole" {
-  yaml_body = file("./manifests/spark-operator-clusterrole.yaml")
+resource "kubectl_manifest" "spark-service-account" {
+  depends_on = [
+    kubectl_manifest.spark-namespace
+  ]
+  count     = length(data.kubectl_path_documents.spark-service-account.documents)
+  yaml_body = element(data.kubectl_path_documents.spark-service-account.documents, count.index)
 }
 
-# Spark ClusterRoleBinding
-resource "kubectl_manifest" "spark_operator_clusterrolebinding" {
-  depends_on = [kubectl_manifest.spark_operator_clusterrole]
-  yaml_body = file("./manifests/spark-operator-clusterrolebinding.yaml")
-}
-
-# GCP Secrets
-resource "kubectl_manifest" "gcp_secrets" {
-  yaml_body = file("./manifests/gcp-secrets.yaml")
-}
 
 # Add Spark-Operator using Helm
-resource "helm_release" "spark" {
-  depends_on = [kubectl_manifest.spark_operator_namespace, kubectl_manifest.spark_operator_serviceaccount, kubectl_manifest.spark_operator_clusterrolebinding, kubectl_manifest.gcp_secrets]
-  name       = "spark"
-  repository = "https://kubeflow.github.io/spark-operator"
-  chart      = "spark-operator"
-  namespace  = kubectl_manifest.spark_operator_namespace.name
-  values = [file("../../applications/spark-operator/values.yaml")]
-  wait       = false
+resource "helm_release" "spark-operator" {
+  name      = "spark-operator"
+  chart     = "../../applications/spark-operator"
+  namespace = "processing"
+  values    = ["${file("../../applications/spark-operator/values.yaml")}"]
+  wait      = false
 }
