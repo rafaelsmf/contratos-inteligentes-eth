b76e08810131f9bbe45bd656aeb4fcaa1f9b180d
rafaelsmf <rafael.sousamf@gmail.com>
2024-10-20 18:38:07 -0300

    fix: resolve issue with gcp-credentials path and postgres url

--- scripts/contratos-inteligentes-eth/jobs/bigquery_to_postgres.py
+++ scripts/contratos-inteligentes-eth/jobs/bigquery_to_postgres.py
@@ -94,7 +94,7 @@ def main(execution_date):
     df_filtered = filter_data_by_date(df, start_date, end_date)
 
     # Conexão com PostgreSQL
-    postgres_url = "jdbc:postgresql://10.106.35.153:5432/crypto_ethereum"
+    postgres_url = "jdbc:postgresql://master-postgres.local/crypto_ethereum"
     postgres_properties = {
         "user": "admin",
         "password": "admin",
