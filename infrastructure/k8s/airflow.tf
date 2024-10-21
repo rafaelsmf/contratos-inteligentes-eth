daed1ca358248d53056ae8ca4dc4c3fd82191885
rafaelsmf <rafael.sousamf@gmail.com>
2024-10-20 23:56:23 -0300

    feat: update airflow chart and resources parameters

--- infrastructure/k8s/airflow.tf
+++ infrastructure/k8s/airflow.tf
@@ -5,16 +5,15 @@ resource "kubectl_manifest" "airflow_namespace" {
 
 # Airflow Secrets
 resource "kubectl_manifest" "airflow_secrets" {
-  yaml_body = file("./manifests/airflow-secrets.yaml")
+  yaml_body = file("./manifests/git-secrets.yaml")
 }
 
 # Add Airflow using Helm
 resource "helm_release" "airflow" {
   depends_on = [kubectl_manifest.airflow_namespace, kubectl_manifest.airflow_secrets]
   name       = "airflow"
-  repository = "https://airflow.apache.org"
-  chart      = "airflow"
+  chart      = "../../applications/airflow"
   namespace  = kubectl_manifest.airflow_namespace.name
-  values = [file("../../applications/airflow/values.yaml")]
-  wait       = false
+  values    = ["${file("../../applications/airflow/values.yaml")}"]
+  wait      = false
 }
